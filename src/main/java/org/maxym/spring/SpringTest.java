a82fe69549b1414cd750091e552ad82485a75700
Maxym <so.unique.december@gmail.com>
2024-02-19 00:25:31 +0200

    lesson 6

--- src/main/java/org/maxym/spring/SpringTest.java
+++ src/main/java/org/maxym/spring/SpringTest.java
@@ -8,9 +8,8 @@ public class SpringTest {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                     "applicationContext.xml");
-        Music music = context.getBean("musicBean", Music.class);
 
-        MusicPlayer player = new MusicPlayer(music);
+        MusicPlayer player = context.getBean("musicPlayer", MusicPlayer.class);
 
         player.playMusic();
 
