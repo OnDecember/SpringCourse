92a23664d403094a081421416a47a95ac18622c6
Maxym <so.unique.december@gmail.com>
2024-02-19 15:07:45 +0200

    lesson 9

--- src/main/java/org/maxym/spring/SpringTest.java
+++ src/main/java/org/maxym/spring/SpringTest.java
@@ -1,5 +1,6 @@
 package org.maxym.spring;
 
+import org.maxym.spring.music.ClassicalMusic;
 import org.maxym.spring.player.MusicPlayer;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -9,25 +10,28 @@ public class SpringTest {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                     "applicationContext.xml");
 
-        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        ClassicalMusic music = context.getBean("classicalMusic", ClassicalMusic.class);
+        System.out.println(music.getSong());
 
-
-        boolean comparison = firstMusicPlayer == secondMusicPlayer;
-
-        System.out.println(comparison);
-
-        System.out.println(firstMusicPlayer);
-        System.out.println(secondMusicPlayer);
-
-        System.out.println(firstMusicPlayer.getMusic().get(0));
-        System.out.println(secondMusicPlayer.getMusic().get(0));
-
-        firstMusicPlayer.setVolume(10);
-
-        System.out.println(firstMusicPlayer.getVolume());
-        System.out.println(secondMusicPlayer.getVolume());
-
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//
+//        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+//
+//        System.out.println(comparison);
+//
+//        System.out.println(firstMusicPlayer);
+//        System.out.println(secondMusicPlayer);
+//
+//        System.out.println(firstMusicPlayer.getMusic().get(0));
+//        System.out.println(secondMusicPlayer.getMusic().get(0));
+//
+//        firstMusicPlayer.setVolume(10);
+//
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
+//
 //        firstMusicPlayer.playMusic();
 //        System.out.println(firstMusicPlayer.getName());
 //        System.out.println(firstMusicPlayer.getVolume());
--- src/main/java/org/maxym/spring/music/ClassicalMusic.java
+++ src/main/java/org/maxym/spring/music/ClassicalMusic.java
@@ -1,8 +1,26 @@
 package org.maxym.spring.music;
 
 public class ClassicalMusic implements Music {
+
+    private String song;
+
+    private ClassicalMusic() {}
+
+    public static ClassicalMusic getClassicalMusic() {
+        ClassicalMusic music = new ClassicalMusic();
+        music.song = "Classical song";
+        return music;
+    }
     @Override
     public String getSong() {
-        return "Classical song";
+        return song;
+    }
+
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
     }
 }
