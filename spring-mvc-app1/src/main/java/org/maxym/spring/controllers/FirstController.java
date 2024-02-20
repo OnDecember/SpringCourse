f409a69f874974ada82e6179b07942c927a1681e
Maxym <so.unique.december@gmail.com>
2024-02-20 14:47:36 +0200

    lesson 21

--- spring-mvc-app1/src/main/java/org/maxym/spring/controllers/FirstController.java
+++ spring-mvc-app1/src/main/java/org/maxym/spring/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package org.maxym.spring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,12 +11,32 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
+    public String helloPage(@RequestParam(value = "name", required = false) String  name,
+                            @RequestParam(value = "surname", required = false) String  surname,
+                            Model model) {
 
-        System.out.println("Hello, " + name + " " + surname);
+        model.addAttribute("message", "Hello, " + name + " " + surname);
 
         return "first/hello";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("a") int a,
+                             @RequestParam("b") int b,
+                             @RequestParam("action") String action,
+                             Model model) {
+
+        String result =  switch (action) {
+            case "multiplication" -> String.valueOf(a * b);
+            case "addition" -> String.valueOf(a + b);
+            case "subtraction" -> String.valueOf(a - b);
+            case "division" -> String.valueOf(a / (double) b);
+            default -> "Incorrect action";
+        };
+
+        model.addAttribute("result", result);
+
+        return "first/calculator";
     }
 
     @GetMapping("/goodbye")
