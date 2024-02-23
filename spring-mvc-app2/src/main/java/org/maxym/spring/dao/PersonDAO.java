9be7ebfe07267032a2ffe9c59497a4580015e6e8
Maxym <so.unique.december@gmail.com>
2024-02-23 23:17:45 +0200

    lesson 59

--- spring-mvc-app2/src/main/java/org/maxym/spring/dao/PersonDAO.java
+++ spring-mvc-app2/src/main/java/org/maxym/spring/dao/PersonDAO.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,21 +27,41 @@ public class PersonDAO {
         return session.createQuery("SELECT p FROM Person p", Person.class).getResultList();
     }
 
+    @Transactional(readOnly = true)
     public Optional<Person> getPerson(String  email) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        List<Person> people = session.createQuery("FROM Person WHERE email = :email", Person.class)
+                .setParameter("email", email).getResultList();
+        if (people.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(people.get(0));
     }
 
+    @Transactional(readOnly = true)
     public Person getPerson(int id) {
-        return null;
+        Session session = sessionFactory.getCurrentSession();
+        return session.get(Person.class, id);
     }
 
+    @Transactional
     public void save(Person person) {
+        Session session = sessionFactory.getCurrentSession();
+        session.persist(person);
     }
 
+    @Transactional
     public void update(Person person, int id) {
+        Session session = sessionFactory.getCurrentSession();
+        person.setId(id);
+        session.merge(person);
     }
 
+    @Transactional
     public void delete(int id) {
+        Session session = sessionFactory.getCurrentSession();
+        Person person = session.get(Person.class, id);
+        session.remove(person);
     }
 
     public void withoutBatch() {
