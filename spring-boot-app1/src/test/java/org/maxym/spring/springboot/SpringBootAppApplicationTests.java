03f9ca3e9cd09528cf9dea15125c691147e85bb5
Maxym <so.unique.december@gmail.com>
2024-02-24 22:57:09 +0200

    lesson 71

---
+++ spring-boot-app1/src/test/java/org/maxym/spring/springboot/SpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maxym.spring.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
