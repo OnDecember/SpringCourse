753594ba0e899a62444d48066b430f5276e7eab4
Maxym <so.unique.december@gmail.com>
2024-02-25 20:03:12 +0200

    lesson 83

--- spring-boot-security-1/src/main/java/org/maxym/spring/springbootsecurity1/service/PersonService.java
+++ spring-boot-security-1/src/main/java/org/maxym/spring/springbootsecurity1/service/PersonService.java
@@ -3,6 +3,7 @@ package org.maxym.spring.springbootsecurity1.service;
 import org.maxym.spring.springbootsecurity1.model.Person;
 import org.maxym.spring.springbootsecurity1.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,10 +14,12 @@ import java.util.Optional;
 public class PersonService {
 
     private final PersonRepository personRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public PersonService(PersonRepository personRepository) {
+    public PersonService(PersonRepository personRepository, PasswordEncoder passwordEncoder) {
         this.personRepository = personRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Optional<Person> findByUsername(String username) {
@@ -25,6 +28,8 @@ public class PersonService {
 
     @Transactional
     public void register(Person person) {
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
+
         personRepository.save(person);
     }
 }
